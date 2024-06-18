bef1f9ca63b189b12e5649c1c7e18cf0e381cbe9
Pratik <pratikyadav3949@gmail.com>
2024-06-18 20:04:27 +0530

    first commit

---
+++ src/test/java/com/TradeWave/TradeWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
