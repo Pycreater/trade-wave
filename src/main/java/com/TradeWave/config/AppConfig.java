941263da4be03eb02f2fbf439496951c56b18743
Pratik <pratikyadav3949@gmail.com>
2024-06-22 19:00:56 +0530

    resolving errors in security

--- src/main/java/com/TradeWave/config/AppConfig.java
+++ src/main/java/com/TradeWave/config/AppConfig.java
@@ -1,22 +1,34 @@
 package com.TradeWave.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
 
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(Authorize -> Authorize
                         .requestMatchers("/api/**")
                         .authenticated()
                         .anyRequest()
-                        .permitAll());
+                        .permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsCofigurationSourse()));
 
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsCofigurationSourse() {
         return null;
     }
 }
---
+++ src/main/java/com/TradeWave/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.TradeWave.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "hdyehdhhdyedhtsttdgekdkjdygfjteafreagfytgaygdygysdyrytaeytyusafhdgyudftyuaefjdshdsyfuiayfudshfua";
+
+    public static final String JWT_HEADER = "Authorization";
+}
